1fbe595de5d84adb0490a30a46f73afd185baa68
Nael Louis <nael.louis1002@gmail.com>
2022-02-10 15:56:59 -0500

    Add catch statement and install dotenv

--- server/mongoose/main.js
+++ server/mongoose/main.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
-require("dotenv").config();
+require('dotenv').config();
 const dbUri = process.env.ATLAS_URI;
+const mongoose = require('mongoose');
 
 (async () => {
+    console.log(dbUri)
     await mongoose.connect(dbUri)
         .then(() => { console.log("Connected to MongoDB") })
+        .catch((err) => {console.error(err.message)})
 })()
 
