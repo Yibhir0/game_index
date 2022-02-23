29aef8f255cf82e11fb547b99426e9c903c77c57
Julian Mora Mesa <morajulian97@gmail.com>
2022-02-22 22:25:31 -0500

    Add db methods for user/game/gamelist queries.

--- server/mongoose/db.js
+++ server/mongoose/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const path = require('path');
 const FeedBack = require('../mongoose/models/feedbackSchema')
+const Game = require('../mongoose/models/gameSchema')
+const User = require('../mongoose/models/userSchema')
 require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 
 module.exports.connectToDB = async () => {
@@ -13,6 +15,12 @@ module.exports.closeDB = async () => {
     await mongoose.connection.close();
 }
 
+module.exports.getFeedback = async (game) => {
+    let feedbacks = await FeedBack.find({ gameID: game._id });
+    return feedbacks;
+
+}
+
 module.exports.deleteFeedBack = async (feedback) => {
     await FeedBack.deleteOne({ _id: feedback._id });
 }
@@ -21,7 +29,84 @@ module.exports.deleteAllFeedBack = async () => {
     await FeedBack.deleteMany();
 }
 
-module.exports.editFeedback = async () => {
-    
+module.exports.editFeedback = async (feedback,query) => {
+    let editedFeedback = await FeedBack.findOneAndUpdate({ _id: feedback._id }, query);
+    return editedFeedback;
 }
 
+module.exports.getGames = async () => {
+    let games = await Game.find();
+    return games;
+}
+
+module.exports.getGame = async (game_name) => {
+    let game = await Game.findOne({ name: game_name });
+    return game;
+}
+
+
+module.exports.getUsers = async () => {
+    let users = await User.find();
+    return users;
+}
+
+module.exports.getUser = async (userName) => {
+    let user = await User.findOne({ name: userName });
+    return user;
+}
+
+module.exports.deleteUser = async (user) => {
+    await User.deleteOne({ _id: user._id});
+}
+
+module.exports.editUser = async (user,query) => {
+    let editedUser = await FeedBack.findOneAndUpdate({ _id: user._id }, query);
+    return editedUser;
+}
+
+module.exports.getLists = async (user) => {
+    let user = await User.findOne({ _id: user._id });
+    return user.lists;
+}
+
+module.exports.getList = async (user, list_id) => {
+    let user = await User.findOne({ _id: user._id });
+    for (let list in user.lists) {
+        if (list['id'] == list_id) {
+            return list['id'];
+        };
+    };
+}
+
+module.exports.createList = async (user, id, list_name, gameslist ) => {
+    await User.findOneAndUpdate({_id: user._id}, {lists : {id: id, name: list_name , games: gameslist}});
+}
+
+module.exports.addToList = async (user, list_name, game_name) => {
+    let user = await User.findOne({ _id: user._id });
+    let lists = user.lists;
+
+    for (let list in lists) {
+        if (list['list_name'] == list_name) {
+            list['games'].push(game_name);
+        }
+    }
+
+    await User.findOneAndReplace({ _id: user_id }, user);
+
+}
+
+module.exports.deleteFromList = async (user, list_name, game_name) => {
+    let user = await User.findOne({ _id: user._id });
+    let lists = user.lists;
+
+    for (let list in lists) {
+        if (list['list_name'] == list_name) {
+            let index = list['games'].indexOf('game_name');
+            array.splice(index, 1);
+        }
+    }
+
+    await User.findOneAndReplace({ _id: user_id }, user);
+    
+}
--- server/mongoose/main.js
+++ server/mongoose/main.js
@@ -1,15 +1,8 @@
-const mongoose = require('mongoose')
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
-const dbUri = process.env.ATLAS_URI;
-const mongoose = require('mongoose');
+const db = require('../mongoose/db');
+const Game = require('../mongoose/models/gameSchema');
 
 
 (async () => {
-    let mng = await mongoose.connect(dbUri)
-        .then(() => { console.log("Connected to MongoDB") })
-    
-    console.log(typeof(mongoose.connection.readyState))
+    await db.connectToDB();
 })()
-
 
