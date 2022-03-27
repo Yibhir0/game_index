d34d5b903deb6f9b05a1dea84a8ea6a96a4c78e7
yassine ibhir <yibhir101@gmail.com>
2022-03-27 11:11:23 -0400

    modify g redirect uri

--- server/app.js
+++ server/app.js
@@ -50,12 +50,14 @@ app.use('/', api);
 
 app.use(cors());
 
+app.use(express.static(path.resolve(__dirname, "../client")));
+
 //app.use(express.static(path.resolve(__dirname, "../client")));
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 // Start listening
 app.listen(PORT, () => {
