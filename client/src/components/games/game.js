f4c2e1eadd76fd35e0fa58f2e5f555e81e99a901
nguyda5 <nguyda2010@gmail.com>
2022-03-28 13:23:50 -0400

    Reformat display of platforms in game detail

--- client/src/components/games/game.js
+++ client/src/components/games/game.js
@@ -34,12 +34,15 @@ const Game = (props) => {
   };
 
   const gameDetails = () => {
+    console.log(props.game);
     let platforms = "";
-    for (let i = 0; i < props.game.platform.length; i++) {
-      if (i === props.game.platform.length - 1) {
-        platforms += props.game.platform[i];
-      } else {
-        platforms += props.game.platform[i] + ", ";
+    if (typeof props.game.platform === "object" ) {
+      for (let i = 0; i < props.game.platform.length; i++) {
+        if (i === props.game.platform.length - 1) {
+          platforms += props.game.platform[i];
+        } else {
+          platforms += props.game.platform[i] + ", ";
+        }
       }
     }
 
