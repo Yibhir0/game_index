3fcb9fd5b927f6eacbf790939fad7e01cd627495
nguyda5 <nguyda2010@gmail.com>
2022-03-28 14:44:19 -0400

    Change topnav to use Group instead of SimpleGrid

--- client/src/components/navbar/topnav.js
+++ client/src/components/navbar/topnav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import {
+    Group,
     Text,
     Anchor,
     Title,
@@ -25,28 +26,20 @@ class TopNav extends Component {
                 <br></br>
                 <nav className="topnav"
                 >
-                    <Grid justify="space-between" align="flex-end" columns={15}>
-                        <Grid.Col span={3}>
-                            <Title> The Game Index </Title>
-                        </Grid.Col>
-                        <Grid.Col span={3}>
-                            <Button className= 'border-gray-500 rounded-lg hover:bg-gray-300/50 active:bg-gray-500/50' variant="subtle" radius="xs">
-                                <Anchor component={Link} to={'/'} >
-                                    Home
-                                </Anchor>
-                            </Button>
-                        </Grid.Col>
-                        <Grid.Col span={3}>
-                            <Button className='border-gray-500 rounded-lg hover:bg-gray-300/50 active:bg-gray-500/50' variant="subtle" radius="xs">
-                                <Anchor component={Link} to={'/games'} >
-                                    Games
-                                </Anchor>
-                            </Button>
-                        </Grid.Col >
-                        <Grid.Col span={3} offset={3}>
-                            <SignIn className="content-end"/>
-                        </Grid.Col>
-                    </Grid>
+                    <Group>
+                        <Title> The Game Index </Title>
+                        <Button className= 'border-gray-500 rounded-lg hover:bg-gray-300/50 active:bg-gray-500/50' variant="subtle" radius="xs">
+                            <Anchor component={Link} to={'/'} >
+                                Home
+                            </Anchor>
+                        </Button>
+                        <Button className='border-gray-500 rounded-lg hover:bg-gray-300/50 active:bg-gray-500/50' variant="subtle" radius="xs">
+                            <Anchor component={Link} to={'/games'} >
+                                Games
+                            </Anchor>
+                        </Button>
+                        <SignIn className="content-end"/>
+                    </Group>
                 </nav>
             </div>
         );
