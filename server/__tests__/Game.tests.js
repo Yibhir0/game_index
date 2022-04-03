a70d1a169b4029f9ec1c660011dcc5ce43ce2853
Nael Louis <nael.louis1002@gmail.com>
2022-04-03 16:50:56 -0400

    Rewrite Test /games and Test /games/:id to pass

---
+++ server/__tests__/Game.tests.js
@@ -0,0 +1,30 @@
+const supertest = require('supertest')
+const app = require("../app")
+const request = supertest(app)
+const db = require('../mongoose/db')
+const router = require('../routes/api')
+
+beforeAll(async () => await db.connectToDB());
+afterAll(async () => await db.closeDB());
+
+describe('Test /games', () => {
+    test('should return the list of games', async () => {
+        const response = await request.get('/api/games')
+        expect(response.status).toEqual(200)
+        expect(response.type).toMatch('application/json')
+        expect(response._body[0].name).toBe('Wii Sports')
+    })
+})
+
+// jest.setTimeout(100000);
+describe('Test /games/:id', () => {
+    test('Should return a specific game', async () => {
+        const response = await request.get('/api/games/623ca8d6daa8ca47ebf7a682')
+        expect(response.status).toBe(200)
+        expect(response.type).toMatch('application/json')
+        expect(response._body.name).toBe('Super Mario Bros.')
+    })
+})
+
+
+
