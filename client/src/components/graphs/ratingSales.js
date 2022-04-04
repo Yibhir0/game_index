5fdfe6753609eabdfc9650fbe58b4265558a617d
Julian Mora Mesa <morajulian97@gmail.com>
2022-04-04 10:47:27 -0400

    Replace FlexibleXYPlot with XYPlot and refactor

--- client/src/components/graphs/ratingSales.js
+++ client/src/components/graphs/ratingSales.js
@@ -4,6 +4,7 @@ import {
     XAxis,
     YAxis,
     FlexibleXYPlot,
+    XYPlot,
     MarkSeries,
     ContourSeries,
     ChartLabel,
@@ -15,59 +16,53 @@ class RatingSales extends Component {
     render() {
 
         return (
-            <FlexibleXYPlot
-                xDomain={[0, 17000000]}
-                getX={d => d.globalsales}
-                getY={d => d.criticscore}
-                margin={{ top: 25 }}
-            >
-                <ContourSeries
+            <XYPlot
+                height={400}
+                width={1200}
+            xDomain={[0, 17000000]}
+            getX={d => d.globalsales}
+            getY={d => d.criticscore}
+            margin={{ top: 25 }}
+        >
+            <ContourSeries
+                className="contour-series-example"
+                style={{
+                    stroke: '#125C77',
+                    strokeLinejoin: 'round'
+                }}
+                colorRange={['#79C7E3', '#FF9833']}
+                data={this.props.ratingSalesGames}
+            />
+                <MarkSeries onValueClick={(datapoint, event) => {
+                    this.props.changeId(datapoint.id);
+                }}
+                data={this.props.ratingSalesGames} size={2} color={'#125C77'} />
+            <Borders style={{ all: { fill: '#fff' } }} />
+            <XAxis tickFormat={v => v / 1000000} style={{ line: { stroke: 'black' } }} />
+            <YAxis style={{ line: { stroke: 'black' } }} />
 
-                    animation
-                    className="contour-series-example"
-                    style={{
-                        stroke: '#125C77',
-                        strokeLinejoin: 'round'
-                    }}
-                    colorRange={['#79C7E3', '#FF9833']}
+            <ChartLabel
+                text="Rating"
+                className="alt-y-label"
+                includeMargin={false}
+                yPercent={0.035}
+                xPercent={0.02}
+                style={{
+                    textAnchor: 'end'
+                }}
+            />
+            <ChartLabel
+                text="Unit Sales (Millions)"
+                className="alt-x-label"
+                includeMargin={false}
+                xPercent={1}
+                yPercent={1.115}
+                style={{
+                    textAnchor: 'end'
+                }}
+            />
+        </XYPlot>
 
-                    data={this.props.ratingSalesGames}
-
-
-                />
-                <MarkSeries
-                    animation
-                    onValueClick={(datapoint, event) => {
-                        this.props.changeId(datapoint.id);
-                    }}
-                    data={this.props.ratingSalesGames} size={4} color={'#125C77'}
-                />
-                <Borders style={{ all: { fill: '#fff' } }} />
-                <XAxis tickFormat={v => v / 1000000} style={{ line: { stroke: 'black' } }} />
-                <YAxis style={{ line: { stroke: 'black' } }} />
-
-                <ChartLabel
-                    text="Rating"
-                    className="alt-y-label"
-                    includeMargin={false}
-                    yPercent={0.035}
-                    xPercent={0.02}
-                    style={{
-                        textAnchor: 'end'
-                    }}
-                />
-                <ChartLabel
-                    text="Unit Sales (Millions)"
-                    className="alt-x-label"
-                    includeMargin={false}
-                    xPercent={1}
-                    yPercent={1.115}
-                    style={{
-                        textAnchor: 'end'
-                    }}
-                />
-
-            </FlexibleXYPlot>
         );
     }
 }
